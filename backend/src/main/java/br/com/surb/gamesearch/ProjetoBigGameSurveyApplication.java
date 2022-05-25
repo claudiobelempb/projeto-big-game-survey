950048a603b23a0107a2d774ee37f4e691d65507
claudiobelempb <claudio.c.lima@hotmail.com>
2022-05-25 08:30:23 -0300

    project start

---
+++ backend/src/main/java/br/com/surb/gamesearch/ProjetoBigGameSurveyApplication.java
@@ -0,0 +1,13 @@
+package br.com.surb.gamesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBigGameSurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBigGameSurveyApplication.class, args);
+	}
+
+}
